66a2683408edc5f492bd4cd400b74c839ad3f79c
Michał Kacprzak <91908197+Michalkacprzak54@users.noreply.github.com>
2024-03-23 15:20:58 +0100

    Add files via upload

---
+++ ConsoleApp/ImportedObject.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp
+{
+    public class ImportedObject
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Schema { get; set; }
+        public string ParentName { get; set; }
+        public string ParentType { get; set; }
+        public string DataType { get; set; }
+        public bool IsNullable { get; set; }
+        public double NumberOfChildren { get; set; }
+    }
+}
